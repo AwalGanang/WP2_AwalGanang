d87f932fdabd7b0a002a79334668a576aaaa6c3f
AwalGanang <Awalganangp@gmail.com>
2023-10-03 20:48:00 +0700

    Selesai Pertemuan 3

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title> Latihan 1</title>
+
+</head>
+
+<body>
+    halo kawan.. yuk kita belajar WEB PROGRAMING..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
